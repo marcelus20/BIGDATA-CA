eafa53ec85d1963703c89e147885404b4f935a82
Marcelus <marcelus20felipe@gmail.com>
2020-04-16 18:46:26 +0100

    added a table header to the div table

--- js/scripts/Utils.js
+++ js/scripts/Utils.js
@@ -9,8 +9,16 @@ class Utils {
         const table = $('<div class="table"></div>');
         arrayOfObjects.forEach((obj, index) => {
             const keys = Object.keys(obj);
+
+            if(!table.is(':parent')){
+                table.append(`<div class="div-th-row">${
+                    keys.map(key=>`<div class="div-th-cell">${key}</div>`)
+                        .reduce((acc,item)=>acc+item,"")
+                }</div>`);
+            }
+
+
             const row = $(`<div class="table-row" id="${keys[0]}${index}"></div>`);
-            
             keys.forEach((key,index_)=>{
                 row.append(`<div class="table-cell" id="${key}${index}x${index_}">${obj[key]}</div>`);
             });
